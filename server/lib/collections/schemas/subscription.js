ac034955c12b05b3b5a78e05a197ad8abb1fd8b4
Daniel Honig <daniel.honig@gmail.com>
2017-09-13 14:19:28 -0400

    .

---
+++ server/lib/collections/schemas/subscription.js
@@ -0,0 +1,36 @@
+import {SimpleSchema } from "meteor/aldeed:simple-schema";
+
+
+export const SubscriptionItem = new SimpleSchema({
+    productId: {
+        type: String,
+        optional: false
+    },
+    variantId: {
+        type: String,
+        optional: true
+    },
+    name: { // Why do we want this?
+        type: String,
+        defaultValue: "Reaction Subscription",
+        label: ""
+    }
+
+});
+
+export const SubscriptionItem = new SimpleSchema({
+
+    Id: {
+        type: String,
+        optional: true
+    },
+    userId: {
+        type: String,
+        unique:true
+    },
+    items: {
+        type: [SubscriptionItem],
+        optional: true
+    }
+
+});
