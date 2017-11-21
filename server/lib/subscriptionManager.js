63238969c8fa4218cfef706126f9ddc62c9095b8
Kenton Newby <kentonnewby@gmail.com>
2017-11-20 22:18:36 -0500

    refactor subscriptionManager to be a class

--- server/lib/subscriptionManager.js
+++ server/lib/subscriptionManager.js
@@ -2,66 +2,68 @@ import {SubscriptionsConfig} from  '../config'
 import {Logger as logger} from "/server/api";
 import {Subscriptions, Plans} from "../../lib/collections";
 
-export const SubscriptionManager = function (name) {
+class SubscriptionManager {
 
-    function getAdapter() {
-        let subscriptionAdapter = SubscriptionsConfig.config.adapters[name];
-        return subscriptionAdapter;
+    constructor(adapter_name) {
+        if (adapter_name == undefined) {
+          throw new TypeError("Need to pass subscription processor to new SubscriptionManager instance. Check Config.config.subscription_processor or ENV variables");
+        } else {
+          this.adapter = new SubscriptionsConfig.config.adapters[adapter_name]();  
+        }
     }
 
-    return {
-        createSubscription: function (customer,paramsHash) {
-            let subscription= getAdapter().createSubscription(customer,paramsHash );
-            let subscriptionReceipt=
-                { id: subscription.id,
-                  userId: paramsHash.user._id,
-                  nextBillingDate: new Date(1000 * subscription.current_period_end),
-                }
-            logger.info("Subscription:", JSON.stringify(subscription));
-            logger.info("Subscription Receipt",subscriptionReceipt);
-            Subscriptions.insert(subscriptionReceipt);
-            //Todo decide on returning gateway response or new object
-            return subscription;
-        },
-        createPlan: function(args) {
-            let plan = getAdapter().createPlan(args);
-
-            /* 
-            shouldn't have to do this, but debugging why I can't just
-            pass in 'plan' from above into the 'Plans.insert()' call below.
-            kept getting the following error:
-                Error: After filtering out keys not in the schema, your object is now empty
-            */
-
-            let planReceipt = {
-                id: plan.id,
-                name: plan.name,
-                currency: plan.currency,
-                amount: plan.amount,
-                interval: plan.interval
+    createSubscription(customer,paramsHash) {
+        let subscription = this.adapter.createSubscription(customer,paramsHash );
+        let subscriptionReceipt=
+            { id: subscription.id,
+                userId: paramsHash.user._id,
+                nextBillingDate: new Date(1000 * subscription.current_period_end),
             }
-            //logger.info("plan created was: ", plan);
-            //Plans.insert(plan);
-            Plans.insert(planReceipt);
-            return plan;
-        },
-        listPlans: function() {
-            let plansList = getAdapter().listPlans();
-            logger.info("plans list: ", plansList);
-            return plansList;
-        },
-        deletePlan: function(planId) {
-            let deletedPlan = getAdapter().deletePlan(planId);
-            return deletedPlan;
-        },
-        fetchTransaction: function (transactionId) {
-            return getAdapter().fetchTransaction(transactionId);
-        },
-        fetchCustomer: function (customerId){
-            return getAdapter().fetchCustomer(customerId);
-       }
- }
+        logger.info("Subscription:", JSON.stringify(subscription));
+        logger.info("Subscription Receipt",subscriptionReceipt);
+        Subscriptions.insert(subscriptionReceipt);
+        //Todo decide on returning gateway response or new object
+        return subscription;
+    }
+    
+    createPlan(args) {
+        let plan = this.adapter.createPlan(args);
+
+        /* 
+        shouldn't have to do this, but debugging why I can't just
+        pass in 'plan' from above into the 'Plans.insert()' call below.
+        kept getting the following error:
+            Error: After filtering out keys not in the schema, your object is now empty
+        */
+
+        let planReceipt = {
+            id: plan.id,
+            name: plan.name,
+            currency: plan.currency,
+            amount: plan.amount,
+            interval: plan.interval
+        }
+
+        Plans.insert(planReceipt);
+        return plan;
+    }
+    listPlans() {
+        let plansList = this.adapter.listPlans();
+        return plansList;
+    }
+
+    deletePlan(planId) {
+        let deletedPlan = this.adapter.deletePlan(planId);
+        return deletedPlan;
+    }
+
+    fetchTransaction(transactionId) {
+        return this.adapter.fetchTransaction(transactionId);
+    }
+
+    fetchCustomer(customerId){
+        return this.adapter.fetchCustomer(customerId);
+    }
 }
 
-
-
+export default SubscriptionManager;
